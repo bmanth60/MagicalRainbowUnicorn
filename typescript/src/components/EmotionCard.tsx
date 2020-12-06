84c17252a8ce2b28b9eab44959c7fe4679de736e
bmanth60 <bman.brianthai@gmail.com>
2020-12-06 15:54:38 -0800

    Update css classes parsing to reflect mui style classes.

--- typescript/src/components/EmotionCard.tsx
+++ typescript/src/components/EmotionCard.tsx
@@ -4,17 +4,19 @@ import React, { PropsWithChildren } from 'react'
 
 import Paper from '@material-ui/core/Paper'
 
-const style = (theme: Theme) =>
-    css({
+const styles = (theme: Theme) => ({
+    root: css({
         backgroundClip: 'padding-box',
         border: '1px solid transparent',
         padding: theme.spacing() * 1,
-    })
+    }),
+})
 
 export default function EmotionCard({ children }: PropsWithChildren<{}>) {
     const theme = useTheme()
+    const classes = styles(theme)
     return (
-        <Paper css={style(theme)} elevation={1}>
+        <Paper css={classes.root} elevation={1}>
             {children}
         </Paper>
     )
